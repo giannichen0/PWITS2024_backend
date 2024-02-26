74ecfca84233308caa18f1b03ef4d2bef056ccbd
Gianni Chen <chengianni38@gmail.com>
2024-02-26 23:43:03 +0100

    added jwt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth/authController")
+const loginLimiter = require("../middleware/loginLimiter")
 
-router.route("/").post()
 
-router.route("/refresh").get()
+router.route("/").post(loginLimiter, authController.login)
 
-router.route("/logout").post()
+router.route("/refresh").get(authController.refresh)
+
+router.route("/logout").post(authController.logout)
 
 
 module.exports = router

d6a644d90006738f07bf1149e038373f14271305
Gianni Chen <chengianni38@gmail.com>
2024-02-26 20:08:18 +0100

    bug fixes

--- controllers/utility/utilityController.js
+++ controllers/utility/utilityController.js
@@ -8,46 +8,37 @@ const { checkId, checkDoctor, checkExam, checkPatient } = require("../../helper/
 
 const emailSender = asyncHandler(async (req, res) => {
   const { doctor, patient, exam } = req.body;
-  if (!doctor || !patient || !exam)
+  if (!doctor || !exam)
     return res
       .status(400)
       .json({
-        message: "missing data. Doctor, patient, and exam are required",
+        message: "missing data. Doctor and exam are required",
       });
 
     
-  if (checkId(doctor))
+  if (!checkId(doctor))
     return res.status(400).json({ message: "invalid doctor id" });
-  if (checkId(patient))
-    return res.status(400).json({ message: "invalid patient id" });
-  if (checkId(exam))
+  //if (checkId(patient)) return res.status(400).json({ message: "invalid patient id" });
+  if (!checkId(exam))
     return res.status(400).json({ message: "invalid exam id" });
 
   const doctorObj = await checkDoctor(doctor);
-  const patientObj = await checkPatient(patient);
   const examObj = await checkExam(exam);
+  const patientObj = await checkPatient(patient) != null ? await checkPatient(patient) : await Patient.findById(examObj.patient).lean().exec()
+  
 
   if (!doctorObj) return res.status(400).json({ message: "doctor not found" });
   if (!patientObj) return res.status(400).json({ message: "patient not found" });
   if (!examObj) return res.status(400).json({ message: "exam not found" });
 
-  if (doctorObj._id.toString() !== patientObj.doctor.toString())
+  if (doctorObj._id.toString() !== patientObj.doctor.toString() || patientObj._id.toString() !== examObj.patient.toString())
     return res
       .status(400)
-      .json({ message: "the doctor must be the same as the patient's doctor" });
-  if (
-    doctorObj._id.toString() !== examObj.doctor.toString() ||
-    patientObj._id.toString() !== examObj.patient.toString()
-  )
-    return res
-      .status(400)
-      .json({
-        message: "the doctor adn the patient must be the same as in exam",
-      });
+      .json({ message: "the doctor must be the same patient's doctor" });
 
-  const timeDifferenceMs = Date.now() - updatedAtDate.getTime();
-  if (!exam.completed && timeDifferenceMs > 60 * 24 * 60 * 60 * 1000) {
-    const email = patient.email;
+  const timeDifferenceMs = Date.now() - examObj.updatedAt.getTime(); 
+  if (!exam.completed && timeDifferenceMs > 60 * 24 * 1000) {
+    const email = patientObj.email;
     const html = `<h1> questo è il primo mail inviato con nodemailer</h1>`;
     const transport = nodemailer.createTransport({
       service: "gmail",
@@ -84,7 +75,8 @@ const emailSender = asyncHandler(async (req, res) => {
       });
   }
 
-  res.status(200).json({message : "the exam does not exceed 60 days"})
+  //res.status(200).json({message : "the exam does not exceed 60 days"})
 });
+
 
 module.exports = { emailSender };

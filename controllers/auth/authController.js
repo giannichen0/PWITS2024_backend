47f4beb19e101fdac23fcfd6ddc315df88f2344f
Gianni Chen <chengianni38@gmail.com>
2024-02-27 09:28:42 +0100

    edit logout

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -132,7 +132,7 @@ const refresh = (req, res) => {
 //@access Public
 const logout = (req, res) => {
     const cookies = req.cookies
-    if (!cookies?.jwt) return res.sendStatus(204) //No content
+    if (!cookies?.jwt) return res.status(204).json({message : "the cookies doesn't exist"}) //No content
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
     res.json({ message: 'Cookie cleared' })
 }

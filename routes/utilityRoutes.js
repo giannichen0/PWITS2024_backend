eb53309f223984fbac65df3d818e488818e9b776
Gianni Chen <chengianni38@gmail.com>
2024-02-27 09:25:20 +0100

    disabling jwt

--- routes/utilityRoutes.js
+++ routes/utilityRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const utilityController = require("../controllers/utility/utilityController")
-const verifyJWT = require("../middleware/verifyJWT")
+// const verifyJWT = require("../middleware/verifyJWT")
 
-router.use(verifyJWT)
+// router.use(verifyJWT)
 
 router.post("/mail", utilityController.emailSender)
 router.get("/pdf", utilityController.pdfGenerator)
